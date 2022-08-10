7353b29531012e99ce8fdc69d50443d94b531bc3
Rutvik081 <kevadia.rutvik08@gmail.com>
2022-08-09 20:05:18 -0400

    Added reference frame for pose goal

--- src/lab_1/src/square.cpp
+++ src/lab_1/src/square.cpp
@@ -94,8 +94,10 @@ int main(int argc, char **argv)
     // Create instance of Plan
     moveit::planning_interface::MoveGroupInterface::Plan plan_pose1;
 
+    std::string reference_frame = "base_link";
+
     bool plan_success;
-    plan_success = ArmController::planToPoseTarget(planning_options,arm_move_group,pose1,plan_pose1);
+    plan_success = ArmController::planToPoseTarget(planning_options,arm_move_group,pose1,reference_frame,plan_pose1);
 
     if(plan_success){
         ROS_INFO("pose 1 plan succeeded");
@@ -179,7 +181,7 @@ int main(int argc, char **argv)
             waypoints.push_back(next_pose4);
         }
 
-        ArmController::planCartesianPath(start_pose, waypoints, arm_move_group);
+        ArmController::planCartesianPath(start_pose, waypoints, reference_frame, arm_move_group);
         
 
     } 
